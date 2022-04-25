56cd38b40a7b054b810b9c2bb9acfdb90e3e8554
vinicius-batista-dev <20201012000256@pucgo.edu.br>
2022-04-25 09:49:00 -0300

    Adicionando o backend

---
+++ pedidoreserva/src/main/java/com/project/backend/pedidoreserva/PedidoreservaApplication.java
@@ -0,0 +1,13 @@
+package com.project.backend.pedidoreserva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidoreservaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidoreservaApplication.class, args);
+	}
+
+}
---
+++ pedidoreserva/src/test/java/com/project/backend/pedidoreserva/PedidoreservaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.backend.pedidoreserva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidoreservaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
