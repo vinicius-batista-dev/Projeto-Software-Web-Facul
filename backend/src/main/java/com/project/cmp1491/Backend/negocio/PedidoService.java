d95637b5ec2ee2946555339302910fef081fc03e
vinicius-batista-dev <20201012000256@pucgo.edu.br>
2022-04-27 18:48:22 -0300

    Implementando o metodo listar em pedido service

--- backend/src/main/java/com/project/cmp1491/Backend/negocio/PedidoService.java
+++ backend/src/main/java/com/project/cmp1491/Backend/negocio/PedidoService.java
@@ -2,6 +2,9 @@ package com.project.cmp1491.Backend.negocio;
 
 import com.project.cmp1491.Backend.entidade.Pedido;
 
+import java.util.List;
+
 public interface PedidoService {
     public Pedido incluir(Pedido pedido);
+    public List<Pedido>listar();
 }
