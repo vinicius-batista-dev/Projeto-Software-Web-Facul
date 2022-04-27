660bb34f5c6c840db90f732b4fd39aa3b0f74e43
vinicius-batista-dev <20201012000256@pucgo.edu.br>
2022-04-27 18:53:40 -0300

    Chamando o pedido service para o PedidoServiceImpl

--- backend/src/main/java/com/project/cmp1491/Backend/negocio/PedidoServiceImpl.java
+++ backend/src/main/java/com/project/cmp1491/Backend/negocio/PedidoServiceImpl.java
@@ -1,4 +1,33 @@
 package com.project.cmp1491.Backend.negocio;
 
-public class PedidoServiceImpl {
+import com.project.cmp1491.Backend.entidade.Pedido;
+
+import java.util.List;
+
+public class PedidoServiceImpl implements PedidoService{
+    
+    @Override
+    public Pedido incluir(Pedido pedido) {
+        return null;
+    }
+
+    @Override
+    public List<Pedido> listar() {
+        return null;
+    }
+
+    @Override
+    public Pedido consultar(Integer id) {
+        return null;
+    }
+
+    @Override
+    public void alterar(Pedido pedido) {
+
+    }
+
+    @Override
+    public void excluir(Integer id) {
+
+    }
 }
