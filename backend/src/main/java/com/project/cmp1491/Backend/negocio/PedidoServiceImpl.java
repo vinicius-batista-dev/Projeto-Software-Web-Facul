46d2b9e2a9353597312c378973572b9aadcef924
vinicius-batista-dev <20201012000256@pucgo.edu.br>
2022-04-27 20:28:33 -0300

    Aplicando o try catch no pedido service impl

--- backend/src/main/java/com/project/cmp1491/Backend/negocio/PedidoServiceImpl.java
+++ backend/src/main/java/com/project/cmp1491/Backend/negocio/PedidoServiceImpl.java
@@ -2,32 +2,56 @@ package com.project.cmp1491.Backend.negocio;
 
 import com.project.cmp1491.Backend.entidade.Pedido;
 
+import java.rmi.server.ExportException;
 import java.util.List;
 
 public class PedidoServiceImpl implements PedidoService{
-    
+
     @Override
     public Pedido incluir(Pedido pedido) {
+        try{
+
+        }catch (Exception err){
+
+        }
         return null;
     }
 
     @Override
     public List<Pedido> listar() {
+        try{
+
+        }catch (Exception err){
+
+        }
         return null;
     }
 
     @Override
     public Pedido consultar(Integer id) {
+        try{
+
+        }catch (Exception err){
+
+        }
         return null;
     }
 
     @Override
     public void alterar(Pedido pedido) {
+        try{
 
+        }catch (Exception err){
+
+        }
     }
 
     @Override
     public void excluir(Integer id) {
+        try {
 
+        }catch (Exception err){
+            
+        }
     }
 }
