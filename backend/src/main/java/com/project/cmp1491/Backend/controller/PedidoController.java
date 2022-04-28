2d33d6e0b7f1158328a44d7665ad5cc7a01eb963
vinicius-batista-dev <20201012000256@pucgo.edu.br>
2022-04-27 21:14:24 -0300

    Aplicando a injeção da variavel pedidoService

--- backend/src/main/java/com/project/cmp1491/Backend/controller/PedidoController.java
+++ backend/src/main/java/com/project/cmp1491/Backend/controller/PedidoController.java
@@ -2,7 +2,18 @@ package com.project.cmp1491.Backend.controller;
 
 import com.project.cmp1491.Backend.negocio.PedidoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class PedidoController {
 
+    private PedidoService pedidoService;
+
+    @Autowired
+    public void setPedidoService(PedidoService pedidoService)
+    {
+        this.pedidoService = pedidoService;
+    }
 }
