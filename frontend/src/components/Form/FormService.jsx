2699dbde8bf6df96f4ef33ea96ffabf90afb7907
vinicius-batista-dev <20201012000256@pucgo.edu.br>
2022-05-04 19:55:21 -0300

    Trabalhando com mask

--- frontend/src/components/Form/FormService.jsx
+++ frontend/src/components/Form/FormService.jsx
@@ -1,14 +1,14 @@
 import { Component } from "react";
 import { TextField } from "@mui/material";
 import Input from "@mui/material";
-import MaskInput from "../Validacao/MaskInput";
+import MaskedInput from "../Validacao/MaskInput";
 
 import React from 'react'
 
 function FormService() {
   return (
     <div className="App">
-        <MaskInput />
+        <MaskedInput />
     </div>
   )
 }
--- frontend/src/components/Validacao/MaskInput.js
+++ frontend/src/components/Validacao/MaskInput.js
@@ -1,7 +1,14 @@
 import React from "react";
+import InputMask from "react-input-mask";  
 
-const MaskInput = () => {
-    return <input />
+
+const MaskedInput = ({value,onChange}) => {
+    return ( 
+        <InputMask 
+            mask="999.999.999-99" 
+            value={value} 
+            onChange={onChange}/>
+    )
 }
 
-export default MaskInput
+export default MaskedInput
