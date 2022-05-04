8f45be6ffd7f8e08725b3eb41c96748ab6fb718a
vinicius-batista-dev <20201012000256@pucgo.edu.br>
2022-05-04 19:16:37 -0300

    Tabalhando com ApiService com metodos padronizados

---
+++ frontend/src/components/ApiServices/ApiService.js
@@ -0,0 +1,20 @@
+import axios from "axios"
+
+const url = 'http://localhost:8080';
+
+axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+
+class ApiServer{
+    listarPedidos(){
+        return axios.get(url + '/listar-pedidos')
+    }
+    consultarPedidoById(pedidoId){
+        return axios.get(url + '/consultar-pedido/' + pedidoId)
+    }
+    salvarPedido(pedido){
+        return axios.post(url + '/salvar-pedido/', pedido);
+    }
+    excluirPedidoById(pedidoId){
+        return axios.delete(url + '/excluir-pedido/' + pedidoId);
+    }
+}
